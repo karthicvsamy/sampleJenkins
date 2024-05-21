522ed4887a31c546622e6a413e1e6b4c16141366
karthicvsamy <karthicvsamy@gmail.com>
2024-05-21 17:29:31 +0530

    First commit

---
+++ JenkinsSample/src/test/java/sample/Package1/SampleClassOne.java
@@ -0,0 +1,18 @@
+package sample.Package1;
+
+import org.testng.annotations.Test;
+
+public class SampleClassOne {
+
+	
+	@Test
+	public void methodOne() {
+		System.out.println("method One class One");
+	}
+	
+	@Test
+	public void methodTwo() {
+		System.out.println("method two class one");
+	}
+	
+}
---
+++ JenkinsSample/src/test/java/sample/Package2/SampleClassTwo.java
@@ -0,0 +1,17 @@
+package sample.Package2;
+
+import org.testng.annotations.Test;
+
+public class SampleClassTwo {
+
+	@Test
+	public void methodOne() {
+		System.out.println("method One class Two");
+	}
+	
+	@Test
+	public void methodTwo() {
+		System.out.println("method two class Two");
+	}
+	
+}
